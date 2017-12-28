264957a3f053363d52c8c39f81c9d14643d1aa84
Harshul Shah <harshulshah@Orange.local>
2017-12-28 16:23:20 +0530

    Initial Commit

---
+++ upload_audioTests/upload_audioTests.swift
@@ -0,0 +1,36 @@
+//
+//  upload_audioTests.swift
+//  upload_audioTests
+//
+//  Created by Harshul Shah on 28/12/17.
+//  Copyright © 2017 Harshul Shah. All rights reserved.
+//
+
+import XCTest
+@testable import upload_audio
+
+class upload_audioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
